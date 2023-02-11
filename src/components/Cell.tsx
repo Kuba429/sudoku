ee22629387037fd6a41d0146dda259a91218f05c
Kuba429 <jakubzawadzki429@gmail.com>
2023-02-11 18:09:47 +0100

    borders

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -15,11 +15,26 @@ export function Cell({
 		if (!cell.canChange) return;
 		setActiveCell({ x: cell.x, y: cell.y });
 	};
-	const activeClass =
-		activeCell?.x === cell.x && activeCell?.y === cell.y ? " active" : "";
 	return (
-		<div onClick={handleClick} className={"cell" + activeClass}>
+		<div
+			onClick={handleClick}
+			className={getCellClasses(cell.x, cell.y, activeCell)}
+		>
 			{cell.value || ""}
 		</div>
 	);
+}
+
+function getCellClasses(
+	x: number,
+	y: number,
+	activeCell: { x: number; y: number } | null
+) {
+	let classes: string[] = ["cell"];
+	if (activeCell?.x === x && activeCell?.y === y) classes.push("active");
+	if (x === 2 || x === 5) classes.push("border-right");
+	if (x === 3 || x === 6) classes.push("border-left");
+	if (y === 3 || y === 6) classes.push("border-top");
+	if (y === 2 || y === 5) classes.push("border-bottom");
+	return classes.join(" ");
 }
